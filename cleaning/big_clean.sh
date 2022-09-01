1b50ff9597de636f4096bb8c0302e931c8868ef6
Ollie Willette <wille667@morris.umn.edu>
2022-09-01 16:00:28 -0500

    Add the big clean bash file, which will extact Big, clean it, and compress to little

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -5,6 +5,10 @@ infi=$1
 SCRATCH=$(mktemp --directory)
 
 tar -zxf "$infi" --directory="$SCRATCH"
+grep -r -l "DELETE ME!" "$SCRATCH" | xargs rm
+home=$(pwd)
 
+cd "$SCRATCH"
 
+tar -zcf "$home/cleaned_little_dir" ./*
 

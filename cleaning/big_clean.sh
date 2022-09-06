38dec60135f6ca7be426b3880ab0101b6990ff94
Ollie Willette <wille667@morris.umn.edu>
2022-09-06 15:06:53 -0500

    Finish up lab making everything pass correctly
    Note the change from ./* to -- *, so now little_dir is not ./little_dir but just little_dir, therefore passing tests

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-infi=$1
+infi=$1 # File given
 
-SCRATCH=$(mktemp --directory)
+SCRATCH=$(mktemp --directory) # Temporary directory
 
-tar -zxf "$infi" --directory="$SCRATCH"
-grep -r -l "DELETE ME!" "$SCRATCH" | xargs rm
-home=$(pwd)
-
-cd "$SCRATCH"
-based=$(basename $infi)
-tar -zcf "$home/cleaned_$based" ./*
+tar -zxf "$infi" --directory="$SCRATCH" 	# Extract the file given to temp
+grep -r -l "DELETE ME!" "$SCRATCH" | xargs rm 	# Find and delete (-r)ecursively files with the (-l)ine "DELETE ME!" 
+						# From inside of scratch
+home=$(pwd)					# Store current place for later
+cd "$SCRATCH" || exit				# Go into scratch
+baseName=$(basename "$infi")			
+tar -zcf "$home/cleaned_$baseName" -- *		# (-z)ip (c)ompress (f)ile to home, chose all (-- *)
 

5d04b494ab4e6cf3e6413007cbf037b3fc50cc4b
Ollie Willette <wille667@morris.umn.edu>
2022-09-06 15:23:16 -0500

    Fix the last bit of shellcheck in extract and complile

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -3,8 +3,8 @@ inputNum=$1 			# take it the first argument
 
 tar -zxf"NthPrime.tgz"		# extract/unzip the file 
 
-cd NthPrime			# go into new directory
-gcc *.c -o NthPrime		# compile all .c files yoinked from (https://stackoverflow.com/questions/19765536/compile-all-c-files-in-a-directory-using-gcc-compiler-in-cmd)
-./NthPrime $inputNum		# run the command NthPrime (inside the folder NthPrime; confusing, sorry)
+cd NthPrime || exit			# go into new directory
+gcc -- *.c -o NthPrime		# compile all .c files yoinked from (https://stackoverflow.com/questions/19765536/compile-all-c-files-in-a-directory-using-gcc-compiler-in-cmd)
+./NthPrime "$inputNum"		# run the command NthPrime (inside the folder NthPrime; confusing, sorry)
 
 

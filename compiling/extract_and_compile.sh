be74f4e8008fd513d21df322f7e4adc0cd8de364
Ollie Willette <wille667@morris.umn.edu>
2022-09-06 15:37:09 -0500

    Fix the compiling issue caused by trying to make shellcheck happy

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -4,7 +4,7 @@ inputNum=$1 			# take it the first argument
 tar -zxf"NthPrime.tgz"		# extract/unzip the file 
 
 cd NthPrime || exit			# go into new directory
-gcc -- *.c -o NthPrime		# compile all .c files yoinked from (https://stackoverflow.com/questions/19765536/compile-all-c-files-in-a-directory-using-gcc-compiler-in-cmd)
+gcc ./*.c -o NthPrime		# compile all .c files yoinked from (https://stackoverflow.com/questions/19765536/compile-all-c-files-in-a-directory-using-gcc-compiler-in-cmd)
 ./NthPrime "$inputNum"		# run the command NthPrime (inside the folder NthPrime; confusing, sorry)
 
 
